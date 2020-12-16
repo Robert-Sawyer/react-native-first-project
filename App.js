853f905eccfcea25ce209303bc0490a90d774a43
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-16 11:39:43 +0100

    Added input and button with styles

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import {StyleSheet, View, Button, TextInput} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <StatusBar style="auto" />
-      <Button title="Change text" onPress={() => setOutputText('The text has changed')}/>
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <View style={styles.row}>
+                <TextInput
+                    placeholder="Dodaj nowy cel"
+                    style={styles.input}
+                />
+                <Button title="DODAJ"/>
+            </View>
+            <View>
+
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        margin: 5,
+    },
 });
