74e54954a881db391dd4b85b90ec8ce7a2930fef
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-12-17 10:24:50 +0100

    Dodanie useState, wyświetlanie celów + style

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import {StyleSheet, View, Button, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, View, Button, TextInput, Text} from 'react-native';
 
 export default function App() {
+    //dodaję stan - wprowadzony cel i funkcję któa go ustawia, oraz listę celów, która za pomocą .map będzie
+    //potem wyświetlana pod inputem
+    const [enteredGoal, setEnteredGoal] = useState('')
+    const [allGoals, setAllGoals] = useState([])
+
+    //w momencie wprowadzania nowego celu w inpucie zmienia się stan i dzięki hookom wpisany cel jest od razu
+    //aktualizowany w stanie i podmieniany jako enteredText
+    const handleGoalInput = (enteredText) => {
+        setEnteredGoal(enteredText)
+    }
+
+    //obsługuję ustawianie celów do tablicy celów - funkcja wewnątrz set... kopiuje poprzednią listę celów,
+    //tworzy nową tablicę, wprowadza do niej starą zawartość i dodaje na końcu świeżo wprowadzony cel
+    const handleAddGoal = () => {
+        setAllGoals(currentGoals => [...currentGoals, enteredGoal])
+    }
+
     return (
         <View style={styles.screen}>
             <View style={styles.row}>
                 <TextInput
+                    {/*handleGoalInput zostaje wywołany w momencie gdy user zaczyna coś wpisywać, a wartość
+                    w inpucie zostaje pokazana jako bieżący stan enteredGoal, czyli aktualnie wprowadzany
+                    tekst*/}
                     placeholder="Dodaj nowy cel"
                     style={styles.input}
+                    onChangeText={handleGoalInput}
+                    value={enteredGoal}
                 />
-                <Button title="DODAJ"/>
+                <Button title="DODAJ" onPress={handleAddGoal}/>
             </View>
             <View>
-
+                {/*<Text> ma ograniczoną możliwośc stylowania, więc dlatego opakowuję go w kolejne View,
+                i dodaję key, ponieważ każdy mapowany element musi posiadać klucz*/}
+                {allGoals.map(goal =>
+                    <View key={goal} style={styles.listItem}>
+                        <Text>{goal}</Text>
+                    </View>
+                )}
             </View>
         </View>
     );
@@ -34,4 +62,11 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 5,
     },
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1,
+    }
 });
